a701de9afc259c5182cf2b03e345c0e000612657
shlublu <shlublu@yahoo.fr>
2020-06-16 22:27:57 +0200

    [Combonatorics] Refactoring

--- tests/math/TestCombinatorics.cpp
+++ tests/math/TestCombinatorics.cpp
@@ -20,7 +20,7 @@ namespace math_Combinatorics
 			constexpr size_t n(5);
 			constexpr size_t k(3);
 
-			const Combination<size_t> combi(n, k);
+			const Combination combi(n, k);
 
 			Assert::AreEqual(n, combi.n());
 			Assert::AreEqual(k, combi.k());
@@ -29,35 +29,35 @@ namespace math_Combinatorics
 
 		TEST_METHOD(CombinationsConstructionThrowsIfKGreaterThanN)
 		{
-			Assert::ExpectException<std::invalid_argument>([]() { return Combination<size_t>(5, 6).number(); });
+			Assert::ExpectException<std::invalid_argument>([]() { return Combination(5, 6).number(); });
 		}
 
 
 		TEST_METHOD(CombinationsHasTheProperNumber)
 		{
-			Assert::AreEqual(size_t(924), Combination<size_t>(12, 6).number());
-			Assert::AreEqual(size_t(5), Combination<size_t>(5, 4).number());
+			Assert::AreEqual(size_t(924), Combination(12, 6).number());
+			Assert::AreEqual(size_t(5), Combination(5, 4).number());
 			
-			Assert::AreEqual(size_t(1), Combination<size_t>(8, 8).number());
+			Assert::AreEqual(size_t(1), Combination(8, 8).number());
 
-			const Combination<size_t> combi(14, 1);
+			const Combination combi(14, 1);
 			Assert::AreEqual(size_t(combi.n()), combi.number());
 		}
 
 
 		TEST_METHOD(CombinationsHasTheProperStaticNumber)
 		{
-			Assert::AreEqual(size_t(924), Combination<size_t>::number(12, 6));
-			Assert::AreEqual(size_t(5), Combination<size_t>::number(5, 4));
+			Assert::AreEqual(size_t(924), Combination::number(12, 6));
+			Assert::AreEqual(size_t(5), Combination::number(5, 4));
 
-			Assert::AreEqual(size_t(1), Combination<size_t>::number(8, 8));
-			Assert::AreEqual(size_t(14), Combination<size_t>::number(14, 1));
+			Assert::AreEqual(size_t(1), Combination::number(8, 8));
+			Assert::AreEqual(size_t(14), Combination::number(14, 1));
 		}
 
 
 		TEST_METHOD(CombinationsRollsAsExepected)
 		{
-			Combination<size_t> combi(12, 6);
+			Combination combi(12, 6);
 
 			size_t ctr(0);
 			while (combi.next())
@@ -70,7 +70,7 @@ namespace math_Combinatorics
 
 		TEST_METHOD(CombinationsIsComplete)
 		{
-			Combination<size_t> combi(5, 4);
+			Combination combi(5, 4);
 
 			Assert::IsTrue(combi.next());
 			Assert::IsTrue(std::vector<size_t>{ {0, 1, 2, 3} } == combi.kUplet());
@@ -100,7 +100,7 @@ namespace math_Combinatorics
 			constexpr size_t n(5);
 			constexpr size_t k(3);
 
-			const Arrangement<size_t> argt(n, k);
+			const Arrangement argt(n, k);
 
 			Assert::AreEqual(n, argt.n());
 			Assert::AreEqual(k, argt.k());
@@ -109,35 +109,35 @@ namespace math_Combinatorics
 
 		TEST_METHOD(ArrangementsConstructionThrowsIfKGreaterThanN)
 		{
-			Assert::ExpectException<std::logic_error>([]() { return Arrangement<size_t>(5, 6).number(); });
+			Assert::ExpectException<std::logic_error>([]() { return Arrangement(5, 6).number(); });
 		}
 
 
 		TEST_METHOD(ArrangementsHasTheProperNumber)
 		{
-			Assert::AreEqual(size_t(665280), Arrangement<size_t>(12, 6).number());
-			Assert::AreEqual(size_t(6), Arrangement<size_t>(3, 2).number());
+			Assert::AreEqual(size_t(665280), Arrangement(12, 6).number());
+			Assert::AreEqual(size_t(6), Arrangement(3, 2).number());
 
-			Assert::AreEqual(size_t(40320), Arrangement<size_t>(8, 8).number());
+			Assert::AreEqual(size_t(40320), Arrangement(8, 8).number());
 
-			const Arrangement<size_t> combi(14, 1);
+			const Arrangement combi(14, 1);
 			Assert::AreEqual(size_t(combi.n()), combi.number());
 		}
 
 
 		TEST_METHOD(ArrangementsHasTheProperStaticNumber)
 		{
-			Assert::AreEqual(size_t(665280), Arrangement<size_t>::number(12, 6));
-			Assert::AreEqual(size_t(6), Arrangement<size_t>::number(3, 2));
+			Assert::AreEqual(size_t(665280), Arrangement::number(12, 6));
+			Assert::AreEqual(size_t(6), Arrangement::number(3, 2));
 
-			Assert::AreEqual(size_t(40320), Arrangement<size_t>::number(8, 8));
-			Assert::AreEqual(size_t(14), Arrangement<size_t>::number(14, 1));
+			Assert::AreEqual(size_t(40320), Arrangement::number(8, 8));
+			Assert::AreEqual(size_t(14), Arrangement::number(14, 1));
 		}
 
 		
 		TEST_METHOD(ArrangementsRollsAsExepected)
 		{
-			Arrangement<size_t> argts(12, 6);
+			Arrangement argts(12, 6);
 
 			size_t ctr(0);
 			while (argts.next())
@@ -151,7 +151,7 @@ namespace math_Combinatorics
 
 		TEST_METHOD(ArrangementsIsComplete)
 		{
-			Arrangement<size_t> argt(3, 2);
+			Arrangement argt(3, 2);
 
 			Assert::IsTrue(argt.next());
 			Assert::IsTrue(std::vector<size_t>{ {0, 1} } == argt.kUplet());
